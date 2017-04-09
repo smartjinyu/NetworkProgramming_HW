3eb0b370c24a9b0fb98653969f0f6d4fc16272db
smartjinyu <smartjinyu@gmail.com>
2017-04-09 13:32:33 +0800

    use read/send instead of sendfile to transfer files
    Former-commit-id: 49b4e742a1cf1deaad4e1b8fed0ec1c6f94f3c39

--- HW_1/client.cpp
+++ HW_1/client.cpp
@@ -21,177 +21,184 @@ int max(int a, int b) {
 }
 
 
-int uploadFile(char* filename,int sockfd){
-    int fd = open(filename,O_RDONLY);
-    if(fd==-1){
-        write(sockfd,"Failed to open file:",20);
-        write(sockfd,strerror(errno),strlen(strerror(errno)));
-        fprintf(stderr,"Failed to open file %s: %s\n",filename,strerror(errno));
+int uploadFile(char *filename, int sockfd) {
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) {
+        write(sockfd, "Failed to open file:", 20);
+        write(sockfd, strerror(errno), strlen(strerror(errno)));
+        fprintf(stderr, "Failed to open file %s: %s\n", filename, strerror(errno));
         return -1;
     }
 
     struct stat stat_buf;// file size to be sent
-    fstat(fd,&stat_buf);
+    fstat(fd, &stat_buf);
     off_t offset = 0;
 
 
-    char header[256]={0};
-    sprintf(header,"put:%s,%d",filename,(int)stat_buf.st_size);
-    write(sockfd,header,sizeof(header));
+    char header[256] = {0};
+    sprintf(header, "put:%s,%d", filename, (int) stat_buf.st_size);
+    write(sockfd, header, sizeof(header));
 
-    int remain_data = (int)stat_buf.st_size;
+    int remain_data = (int) stat_buf.st_size;
     ssize_t len;
-    while((len=sendfile(sockfd,fd,&offset,MAXLINE))>0){
+    while ((len = sendfile(sockfd, fd, &offset, MAXLINE)) > 0) {
         remain_data -= len;
     }
+    write(sockfd, "ok\n", 3);
     close(fd);
     return 0;
 }
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
-void str_cli(FILE *fp,int sockfd){
-    int maxfdp1,stdineof;
+
+void str_cli(FILE *fp, int sockfd) {
+    int maxfdp1, stdineof;
     fd_set rset;
-    char sendline[MAXLINE],recvline[MAXLINE];
-    bzero(sendline,sizeof(sendline));
-    bzero(recvline,sizeof(recvline));
+    char sendline[MAXLINE], recvline[MAXLINE];
+    bzero(sendline, sizeof(sendline));
+    bzero(recvline, sizeof(recvline));
     FD_ZERO(&rset);
     stdineof = 0; // use for test readable, 0:connect
     while (true) {
-        if(stdineof == 0){
-            FD_SET(sockfd, &rset);// socket
+        if (stdineof == 0) {
+            FD_SET(fileno(fp), &rset);// input
         }
-        FD_SET(fileno(fp), &rset);// input
+        FD_SET(sockfd, &rset);// socket
 
         maxfdp1 = max(fileno(fp), sockfd) + 1;
         select(maxfdp1, &rset, NULL, NULL, NULL);
 
         if (FD_ISSET(sockfd, &rset)) {
             // socket is readable
-            if (recv(sockfd, recvline, MAXLINE,0) == 0) {
+            if (recv(sockfd, recvline, MAXLINE, 0) == 0) {
+                printf("recv = 0\n");
                 if (stdineof == 1) {
                     return; // normal termination
                 } else {
                     printf("Client: Server terminated prematurely");
                     exit(-1);
                 }
+
+
             }
 
-            if(recvline[0]=='g' && recvline[1]=='e' && recvline[2]=='t'){
+            if (recvline[0] == 'g' && recvline[1] == 'e' && recvline[2] == 't') {
                 // download file from server
                 // recvline: get:test.c,255
 
-                char filename[256]={0},filesize[256]={0};
+                char filename[256] = {0}, filesize[256] = {0};
                 int file_size = 0;
                 int j;
-                for(j=4;recvline[j]!=',';j++){
-                    filename[j-4]=recvline[j];
+                for (j = 4; recvline[j] != ','; j++) {
+                    filename[j - 4] = recvline[j];
                 }
                 j++;
                 int k;
-                for(k=0;recvline[j]!=0;j++,k++){
-                    filesize[k]=recvline[j];
+                for (k = 0; recvline[j] != 0; j++, k++) {
+                    filesize[k] = recvline[j];
                 }
                 file_size = atoi(filesize);
-                //printf("filename = %s, filesize = %d\n",filename,file_size);
-                
-                FILE *recvFile;
-                recvFile = fopen(filename,"w");
-                if(recvFile != NULL){
-                    ssize_t len;
-                    bzero(recvline,sizeof(recvline));
-                    int received_data = 0;
-                    while(1){
-                        //printf("recv started!\n");
+                printf("filename = %s, filesize = %d\n",filename,file_size);
 
-                        len = recv(sockfd,recvline,MAXLINE,0);
+                FILE *recvFile;
+                recvFile = fopen(filename, "w");
+                if (recvFile != NULL) {
+                    ssize_t len;
+                    bzero(recvline, sizeof(recvline));
+                    int received_data = 0;
+                    while (1) {
+                        //printf("recv started!\n");
+                        len = recv(sockfd, recvline, MAXLINE, 0);
+                        if (len <= 0) {
+                            break;
+                        }
+
+
+
                         //printf("recv completed!\n");
-                        if(len<=0){
-                            break;
-                        }
-                        received_data+=len;
-                        //printf("len = %d, received = %d\n",len,received_data);
-                        fwrite(recvline,sizeof(char),len,recvFile);
+                        received_data += len;
+                        printf("len = %d, received = %d\n",len,received_data);
+                        fwrite(recvline, sizeof(char), len, recvFile);
                         //printf("fwrite completed!\n");
-                        if(received_data >= file_size){
+                        if (received_data >= file_size) {
                             break;
                         }
-                        bzero(recvline,sizeof(recvline));
+                        bzero(recvline, sizeof(recvline));
                     }
                     printf("Download Complete!\n");
                     fclose(recvFile);
-                }else{
-                    fputs("error when opening file",stderr);
+                } else {
+                    fputs("error when opening file", stderr);
                 }
 
-            }else{
+            } else {
                 fputs(recvline, stdout);
                 fflush(stdout);
                 // printf("%s\n",recvline);
             }
             fflush(stdout);
-            bzero(recvline,sizeof(recvline));
+            bzero(recvline, sizeof(recvline));
 
         }
 
         if (FD_ISSET(fileno(fp), &rset)) {
             // input is readable
-            if (FD_ISSET(fileno(fp), &rset)) {
 
-                if(fgets(sendline,MAXLINE,fp)==NULL){
-                    //EOF
-                    stdineof = 1;
-                    shutdown(sockfd, SHUT_WR);// half close, close write connection here (client -> server)
-                    printf("half closed\n");
-                    FD_CLR(fileno(fp), &rset);
-                    continue;
-
-                }
-                
-
-                if(sendline[0]=='p' && sendline[1]=='u' && sendline[2]=='t'){
-                    // put 1.pdf or put /home/1.pdf
-                    char filename[128]={0};
-                    int lastslash = 3;
-                    
-                    for (int j = 4; sendline[j] != '\n' && sendline[j] != '\000'; j++) {
-                        //filename[j - 4] = sendline[j];
-                        if(sendline[j]=='/'){
-                            lastslash = j;
-                        }
-                    }
-                    lastslash++;
-                    for (int j = lastslash; sendline[j] != '\n' && sendline[j] != '\000'; j++) {
-                        filename[j - lastslash] = sendline[j];
-                    }
-                    printf("filename = %s\n",filename);
-                    uploadFile(filename,sockfd);
-                    
-                }else{
-                    write(sockfd, sendline, strlen(sendline));
-                }
-                bzero(sendline,sizeof(sendline));
+            if (read(fileno(fp), sendline, MAXLINE) == NULL) {
+                //EOF
+                stdineof = 1;
+                shutdown(sockfd, SHUT_WR);// half close, close write connection here (client -> server)
+                printf("half closed\n");
+                FD_CLR(fileno(fp), &rset);
+                continue;
 
             }
+
+
+            if (sendline[0] == 'p' && sendline[1] == 'u' && sendline[2] == 't') {
+                // put 1.pdf or put /home/1.pdf
+                char filename[128] = {0};
+                int lastslash = 3;
+
+                for (int j = 4; sendline[j] != '\n' && sendline[j] != '\000'; j++) {
+                    //filename[j - 4] = sendline[j];
+                    if (sendline[j] == '/') {
+                        lastslash = j;
+                    }
+                }
+                lastslash++;
+                for (int j = lastslash; sendline[j] != '\n' && sendline[j] != '\000'; j++) {
+                    filename[j - lastslash] = sendline[j];
+                }
+                printf("filename = %s\n", filename);
+                uploadFile(filename, sockfd);
+
+            } else {
+                write(sockfd, sendline, strlen(sendline));
+            }
+            bzero(sendline, sizeof(sendline));
+
         }
+
     }
 }
+
 #pragma clang diagnostic pop
 
-int main(int argc,char **argv){
+int main(int argc, char **argv) {
     int sockfd;
     struct sockaddr_in serverAddr;
-    if(argc!=3){
+    if (argc != 3) {
         printf("wrong arguments! Please run ./client.out <ip> <port> \n");
         return -1;
     }
-    sockfd = socket(AF_INET,SOCK_STREAM,0);
-    bzero(&serverAddr,sizeof(serverAddr));
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&serverAddr, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons((uint16_t)atoi(argv[2]));// port
-    inet_pton(AF_INET,argv[1],&serverAddr.sin_addr);
-    connect(sockfd,(struct sockaddr *)&serverAddr,sizeof(serverAddr));
-    str_cli(stdin,sockfd);
+    serverAddr.sin_port = htons((uint16_t) atoi(argv[2]));// port
+    inet_pton(AF_INET, argv[1], &serverAddr.sin_addr);
+    connect(sockfd, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+    str_cli(stdin, sockfd);
     return 0;
 }
--- HW_1/server.cpp
+++ HW_1/server.cpp
@@ -16,82 +16,82 @@
 
 #define MAXLINE 2048
 #define LISTENQ 1024
-char curDir [FD_SETSIZE][128];
-char defaultDir [128];
+char curDir[FD_SETSIZE][128];
+char defaultDir[128];
 
 int listFiles(int sockfd) {
-    char buf[MAXLINE]={0};
+    char buf[MAXLINE] = {0};
     FILE *fp;
 
     //bzero(&buf,sizeof(buf));
 
-    if(getcwd(buf,sizeof(buf))!=NULL){
-        write(sockfd,"List files in ",14);
-        write(sockfd,buf,strlen(buf));
-        write(sockfd,"\n",1);
-    }else{
-        write(sockfd,"Something went wrong...\n",24);
+    if (getcwd(buf, sizeof(buf)) != NULL) {
+        write(sockfd, "List files in ", 14);
+        write(sockfd, buf, strlen(buf));
+        write(sockfd, "\n", 2);
+    } else {
+        write(sockfd, "Something went wrong...\n", 25);
         return -1;
     }
     if ((fp = popen("ls -a", "r")) == NULL) {
-        write(sockfd,"Something went wrong...\n",24);
+        write(sockfd, "Something went wrong...\n", 25);
         return -1;
     }
 
-    bzero(&buf,sizeof(buf));
+    bzero(&buf, sizeof(buf));
     while (fgets(buf, MAXLINE, fp) != NULL) {
-        write(sockfd,buf,strlen(buf));
+        write(sockfd, buf, strlen(buf));
     }
 
-    if(pclose(fp))  {
+    if (pclose(fp)) {
         return -1;
     }
     return 0;
 }
 
-int changeDir(char path[],int sockfd,int i){
-    if(chdir(path)==0){
-        bzero(&curDir[i],sizeof(curDir[i]));
-        if(getcwd(curDir[i],sizeof(curDir[i]))!=NULL){
-            write(sockfd,"Current path is ",16);
-            write(sockfd,curDir[i],strlen(curDir[i]));
-            write(sockfd,"\n",1);
-        }else{
-            write(sockfd,"Something went wrong...\n",24);
+int changeDir(char path[], int sockfd, int i) {
+    if (chdir(path) == 0) {
+        bzero(&curDir[i], sizeof(curDir[i]));
+        if (getcwd(curDir[i], sizeof(curDir[i])) != NULL) {
+            write(sockfd, "Current path is ", 16);
+            write(sockfd, curDir[i], strlen(curDir[i]));
+            write(sockfd, "\n", 2);
+        } else {
+            write(sockfd, "Something went wrong...\n", 25);
             return -1;
         }
         return 0;
-    }else{
-        write(sockfd,"Something went wrong...\n",24);
+    } else {
+        write(sockfd, "Something went wrong...\n", 25);
         return -1;
     }
 }
 
-int downloadFile(char* filename,int sockfd,int i){
-    char currentDir[256]={0};
-    getcwd(currentDir,sizeof(currentDir));
-    printf("Current dir is %s\n",currentDir);
-
-    int fd = open(filename,O_RDONLY | O_NONBLOCK);
-    if(fd==-1){
-        write(sockfd,"Failed to open file:",20);
-        write(sockfd,strerror(errno),strlen(strerror(errno)));
-        fprintf(stderr,"Failed to open file %s: %s\n",filename,strerror(errno));
+int downloadFile(char *filename, int sockfd, int i) {
+    char currentDir[256] = {0};
+    getcwd(currentDir, sizeof(currentDir));
+    printf("Current dir is %s\n", currentDir);
+    /*
+    int fd = open(filename, O_RDONLY | O_NONBLOCK);
+    if (fd == -1) {
+        write(sockfd, "Failed to open file:", 20);
+        write(sockfd, strerror(errno), strlen(strerror(errno))+1);
+        fprintf(stderr, "Failed to open file %s: %s\n", filename, strerror(errno));
         return -1;
     }
     struct stat stat_buf;// file size to be sent
-    fstat(fd,&stat_buf);
+    fstat(fd, &stat_buf);
     off_t offset = 0;
-    
 
-    char header[256]={0};
-    sprintf(header,"get:%s,%d",filename,(int)stat_buf.st_size);
-    write(sockfd,header,sizeof(header));
-    
-    int remain_data = (int)stat_buf.st_size;
+
+    char header[256] = {0};
+    sprintf(header, "get:%s,%d", filename, (int) stat_buf.st_size);
+    write(sockfd, header, strlen(header)+1);
+
+    int remain_data = (int) stat_buf.st_size;
     //printf("filesize = %d\n",remain_data);
     ssize_t len;
-    while((len=sendfile(sockfd,fd,&offset,MAXLINE))>0){
+    while ((len = sendfile(sockfd, fd, &offset, stat_buf.st_size)) > 0) {
         remain_data -= len;
         //printf("len = %d, remaining = %d\n",len,remain_data);
 
@@ -99,49 +99,79 @@ int downloadFile(char* filename,int sockfd,int i){
     //printf("last len = %d\n",len);
     close(fd);
     //printf("send successfully!\n");
+
+    */
+    struct stat st;
+    stat(filename,&st);
+    char header[256] = {0};
+    sprintf(header, "get:%s,%d", filename, (int)st.st_size);
+    printf("filesize = %d\n",(int)st.st_size);
+    write(sockfd, header, strlen(header)+1);
+    int f = open(filename,O_RDONLY);
+    if(f<0){
+        perror(filename);
+        return -1;
+    }
+    int sent = 0, remain = (int) st.st_size;
+    printf("sending files... \n");
+    ssize_t read_bytes,sent_bytes;
+    char sendbuf[MAXLINE]={0};
+    while((read_bytes = read(f,sendbuf,MAXLINE))>0){
+        if((sent_bytes = send(sockfd,sendbuf,(size_t)read_bytes,0))<read_bytes){
+            perror("send error");
+            return -1;
+        }
+        sent += sent_bytes;
+        remain -= sent_bytes;
+        printf("send = %d, remaining = %d \n",sent_bytes ,remain);
+    }
+    close(f);
+    printf("Sent successfully\n");
+
+
     return 0;
 }
 
 
-int uploadFile(char* header,int sockfd,int i){
+int uploadFile(char *header, int sockfd, int i) {
     // download file from server
     // recvline: get:test.c,255
 
-    char filename[256]={0},filesize[256]={0};
+    char filename[256] = {0}, filesize[256] = {0};
     int file_size = 0;
     int j;
-    for(j=4;header[j]!=',';j++){
-        filename[j-4]=header[j];
+    for (j = 4; header[j] != ','; j++) {
+        filename[j - 4] = header[j];
     }
     j++;
     int k;
-    for(k=0;header[j]!=0;j++,k++){
-        filesize[k]=header[j];
+    for (k = 0; header[j] != 0; j++, k++) {
+        filesize[k] = header[j];
     }
     file_size = atoi(filesize);
-    
-    printf("filename = %s, filesize = %d\n",filename,file_size);
-    
-    char recvline[MAXLINE+1]={0};
+
+    printf("filename = %s, filesize = %d\n", filename, file_size);
+
+    char recvline[MAXLINE + 1] = {0};
     FILE *recvFile;
-    recvFile = fopen(filename,"w");
+    recvFile = fopen(filename, "w");
     ssize_t len;
-    bzero(recvline,sizeof(recvline));
+    bzero(recvline, sizeof(recvline));
     int received_data = 0;
-    
-    while(1){
-        len = read(sockfd,recvline,MAXLINE);
-        if(len<=0){
+
+    while (1) {
+        len = read(sockfd, recvline, MAXLINE);
+        if (len <= 0) {
             break;
         }
-        received_data+=len;
-        fwrite(recvline,sizeof(char),len,recvFile);
-        if(received_data >= file_size){
+        received_data += len;
+        fwrite(recvline, sizeof(char), len, recvFile);
+        if (received_data >= file_size) {
             break;
         }
     }
     printf("Upload Complete!\n");
-    write(sockfd,"Upload Complete!\n",17);
+    write(sockfd, "Upload Complete!\n", 17);
     fclose(recvFile);
     return 0;
 
@@ -150,138 +180,141 @@ int uploadFile(char* header,int sockfd,int i){
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
-int main(int argc, char **argv){
-    int listenid,connfd,sockfd,i,maxi,maxfd;
-    int nready,client[FD_SETSIZE];
-    fd_set rset,allset;
+
+int main(int argc, char **argv) {
+    int listenfd, connfd, sockfd, i, maxi, maxfd;
+    int nready, client[FD_SETSIZE];
+    fd_set rset, allset;
     ssize_t n;
     char recvline[MAXLINE];
-    struct sockaddr_in serverAddr,clientAddr;
+    struct sockaddr_in serverAddr, clientAddr;
     socklen_t clientLen;
 
-    if(argc!=2){
+    if (argc != 2) {
         printf("wrong arguments! Please run ./server.out <port> \n");
         return -1;
     }
 
-    listenid = socket(AF_INET,SOCK_STREAM,0);
-    bzero(&serverAddr,sizeof(serverAddr));
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&serverAddr, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);// for any interface on the server
-    serverAddr.sin_port = htons((uint16_t)atoi(argv[1]));
+    serverAddr.sin_port = htons((uint16_t) atoi(argv[1]));
 
-    bind(listenid,(struct sockaddr *)&serverAddr,sizeof(serverAddr));
+    bind(listenfd, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
     // monitor the request on the port
-    listen(listenid,LISTENQ);
+    listen(listenfd, LISTENQ);
     // LISTENQ the max length of the backlog (= complete + incomplete request queue)
-    for(int j=0;j<FD_SETSIZE;j++){
-        bzero(curDir[j],sizeof(curDir[j]));
+    for (int j = 0; j < FD_SETSIZE; j++) {
+        bzero(curDir[j], sizeof(curDir[j]));
     }
-    bzero(defaultDir,sizeof(defaultDir));
-    getcwd(defaultDir,sizeof(defaultDir));
+    bzero(defaultDir, sizeof(defaultDir));
+    getcwd(defaultDir, sizeof(defaultDir));
 
-    maxfd = listenid;
+    maxfd = listenfd;
     maxi = -1;
-    for(i=0;i<FD_SETSIZE;i++){
-        client[i]=-1;
+    for (i = 0; i < FD_SETSIZE; i++) {
+        client[i] = -1;
     }
     FD_ZERO(&allset);
-    FD_SET(listenid,&allset);
-    while(true){
-        rset=allset;
-        nready = select(maxfd+1,&rset,NULL,NULL,NULL);
+    FD_SET(listenfd, &allset);
+    while (true) {
+        rset = allset;
+        nready = select(maxfd + 1, &rset, NULL, NULL, NULL);
         // number of ready descriptors
-        if(FD_ISSET(listenid,&rset)){
+        if (FD_ISSET(listenfd, &rset)) {
             clientLen = sizeof(clientAddr);
-            connfd = accept(listenid,(struct sockaddr *) &clientAddr,&clientLen);
+            connfd = accept(listenfd, (struct sockaddr *) &clientAddr, &clientLen);
             printf("New client connected, ip = %s, port = %d\n",
-                   inet_ntoa(clientAddr.sin_addr),ntohs(clientAddr.sin_port));
+                   inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
             // new client connection
-            for(i=0;i<FD_SETSIZE;i++){
-                if(client[i]<0){
+            for (i = 0; i < FD_SETSIZE; i++) {
+                if (client[i] < 0) {
                     // find the first available descriptor
-                    client[i]=connfd;
-                    strcpy(curDir[i],defaultDir);
+                    client[i] = connfd;
+                    strcpy(curDir[i], defaultDir);
                     break;
                 }
             }
-            if(i==FD_SETSIZE){
+            if (i == FD_SETSIZE) {
                 printf("too many clients\n");
             }
-            FD_SET(connfd,&allset);
-            if(connfd > maxfd){
+            FD_SET(connfd, &allset);
+            if (connfd > maxfd) {
                 maxfd = connfd;
             }
-            if(i>maxi){
+            if (i > maxi) {
                 maxi = i;
             }
-            if(--nready<0){
+            if (--nready <= 0) {
                 continue;// no more readable descriptors
             }
 
         }
 
-        for(i=0;i<=maxi;i++){
+        for (i = 0; i <= maxi; i++) {
             // check all clients for data
             if ((sockfd = client[i]) < 0) {
                 continue; // skip empty client
             }
             if (FD_ISSET(sockfd, &rset)) {
-                bzero(&recvline,sizeof(recvline));
+                bzero(&recvline, sizeof(recvline));
                 if ((n = read(sockfd, recvline, MAXLINE)) == 0) {
                     // connection closed by client
                     struct sockaddr_in terminatedAddr;
                     socklen_t len = sizeof(terminatedAddr);
-                    getpeername(sockfd,(struct sockaddr *)&terminatedAddr,&len);
+                    getpeername(sockfd, (struct sockaddr *) &terminatedAddr, &len);
                     printf("Client terminated, ip = %s, port = %d\n",
-                           inet_ntoa(terminatedAddr.sin_addr),ntohs(terminatedAddr.sin_port));
+                           inet_ntoa(terminatedAddr.sin_addr), ntohs(terminatedAddr.sin_port));
 
                     close(sockfd);
                     FD_CLR(sockfd, &allset);
                     client[i] = -1;
-                    bzero(curDir[i],sizeof(curDir[i]));
+                    bzero(curDir[i], sizeof(curDir[i]));
 
                 } else {
-                    //write(sockfd, recvline, (size_t)n);
                     fputs(recvline, stdout);
                     fflush(stdout);
-                    if(recvline[0]=='l' && recvline[1]=='s'){
+
+                    //write(sockfd, recvline, (size_t) n);
+                    if (recvline[0] == 'l' && recvline[1] == 's') {
                         // list files
                         chdir(curDir[i]);
                         listFiles(sockfd);
-                    }else if(recvline[0]=='c' && recvline[1]=='d') {
+                    } else if (recvline[0] == 'c' && recvline[1] == 'd') {
                         // change directory;
                         chdir(curDir[i]);
-                        char dir[128]={0};
+                        char dir[128] = {0};
                         bzero(&dir, sizeof(dir));
                         int j;
                         for (j = 3; recvline[j] != '\n' && recvline[j] != '\000'; j++) {
                             dir[j - 3] = recvline[j];
                         }
                         changeDir(dir, sockfd, i);
-                    }else if(recvline[0]=='g' && recvline[1]=='e' && recvline[2]=='t'){
+                    } else if (recvline[0] == 'g' && recvline[1] == 'e' && recvline[2] == 't') {
                         // download file from server
                         chdir(curDir[i]);
-                        char filename[128]={0};
+                        char filename[128] = {0};
                         for (int j = 4; recvline[j] != '\n' && recvline[j] != '\000'; j++) {
                             filename[j - 4] = recvline[j];
                         }
-                        downloadFile(filename,sockfd,i);
-                    }else if(recvline[0]=='p' && recvline[1]=='u' && recvline[2]=='t'){
+                        downloadFile(filename, sockfd, i);
+                    } else if (recvline[0] == 'p' && recvline[1] == 'u' && recvline[2] == 't') {
                         // upload file to server
                         chdir(curDir[i]);
-                        uploadFile(recvline,sockfd,i);
-                    }else{
+                        uploadFile(recvline, sockfd, i);
+                    } else {
 //                        fputs(recvline, stdout);
 //                        fflush(stdout);
                     }
                 }
-            }
-            if (--nready < 0) {
-                break; // no more readable descriptors
-            }
+                if (--nready <= 0) {
+                    break; // no more readable descriptors
+                }
 
+            }
         }
     }
 }
+
 #pragma clang diagnostic pop
